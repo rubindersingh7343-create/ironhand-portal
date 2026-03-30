b0e8ef38d5938ef8d896188442d018340e45e58c
Rubin Singh <rubindersingh7343@gmail.com>
2026-03-30 08:42:48 -0700

    Fix iOS launch black flash

--- mobile/ios/App/App/AppViewController.swift
+++ mobile/ios/App/App/AppViewController.swift
@@ -2,4 +2,19 @@ import UIKit
 import Capacitor
 
 @objc(AppViewController)
-class AppViewController: CAPBridgeViewController {}
+class AppViewController: CAPBridgeViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Avoid the brief black flash while the WKWebView spins up / loads remote content.
+    // Keep this aligned with the web app boot screen background (#071327).
+    let brandBackground = UIColor(red: 7.0/255.0, green: 19.0/255.0, blue: 39.0/255.0, alpha: 1.0)
+    view.backgroundColor = brandBackground
+
+    // CAPBridgeViewController exposes the underlying web view via the bridge.
+    let webView = bridge?.webView
+    webView?.isOpaque = false
+    webView?.backgroundColor = brandBackground
+    webView?.scrollView.backgroundColor = brandBackground
+  }
+}
